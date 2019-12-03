98718e1e15bb687186a46e3fad39f1ac000eec29
dkuku <daniel.kuku@gmail.com>
2019-12-03 22:13:42 +0000

    day3 part 1

--- lib/aoc2019.ex
+++ lib/aoc2019.ex
@@ -8,60 +8,53 @@ defmodule Aoc2019 do
 
   ## Examples
   """
-  def day1 do
-    File.read!("data/01.txt")
+  def read_day31 do
+    File.read!("data/03.txt")
+    |> day31
+  end
+  def day31(string) do
+    starting_point = {0, 0}
+    [l1 | [l2]] = string
     |> String.split("\n", trim: true)
-    |> Enum.map(&String.to_integer/1)
-    #|> Enum.reduce(0, &mass(&1) + &2) #first solution
-    |> Enum.reduce(0, &mass_rec(&1) + &2)
+    |> Enum.map(&String.split(&1, ","))
+    #map over both lists
+    |> Enum.map(&process_one_list(&1))
+    |> Enum.map(&Enum.into(&1, MapSet.new()))
+    MapSet.intersection(l1, l2)
+    |> MapSet.to_list()
+    |> Enum.sort_by(fn {x,y} -> abs(x)+abs(y)   end)
+    |> Enum.fetch!(1)
+    |> Tuple.to_list
+    |> Enum.sum
   end
-
-  def mass(num), do: div(num,3) - 2
-  def mass_rec(num) when num < 9, do: 0
-  def mass_rec(num), do: mass(num) + mass_rec(mass(num))
-  
-  def string_to_list(string) do
-    string
-    |> String.trim()
-    |> String.split(",")
-    |> Enum.map(&String.to_integer/1)
-  end
-  def list_to_map(list) do
+  def process_one_list(list) do
     list
-    |> Enum.with_index()
-    |> Enum.map(fn {k,v}->{v,k} end) 
-    |> Map.new
+    |> Enum.reduce([{0,0}], &gen_line(&1, &2))
+
+    #  Enum.reduce(nested_list, [], &gen_line(&1, &2, acc))
+    #|> Enum.reduce(0, &mass_rec(&1) + &2)
   end
 
-  def day2() do
-    Enum.each(0..99, fn x -> 
-      Enum.each(0..99, fn y -> day2(x,y) end)
-    end)
+  def gen_end(start, "+", len), do: start + String.to_integer(len)
+  def gen_end(start, "-", len), do: start - String.to_integer(len)
+  def gen_line("R" <> length, [{x,y}| _] = acc) do
+    head = gen_end(x, "+", length)..x+1
+    |> Enum.map(&{&1, y})
+    head ++ acc
   end
-  def day2(noun, verb) do
-    list = File.read!("data/02.txt")
-            |> string_to_list()
-    len = div(length(list), 4)
-    res = list_to_map(list)
-    |> Map.put(1, noun)
-    |> Map.put(2, verb)
-    |> day2_process(len)
-    if res == 19690720 do
-      IO.inspect(res)
-      IO.inspect(noun)
-      IO.inspect(verb)
-    end
-    res
+  def gen_line("L" <> length,[{x,y}|_] = acc) do
+    head = gen_end(x, "-", length)..x-1
+    |> Enum.map(&{&1, y})
+    head ++ acc
   end
-  def day2_process(ma, len) do
-    Enum.reduce_while(0..len-1, ma, fn n, map -> 
-      case map[n*4] do
-        1 -> {:cont, %{map | map[n*4 + 3] => map[map[n*4 + 1]] + map[map[n*4 + 2]]}}
-        2 -> {:cont, %{map | map[n*4 + 3] => map[map[n*4 + 1]] * map[map[n*4 + 2]]}}
-        99 -> {:halt, map[0]}
-        _ -> {:halt, map}
-      end
-    end)
-
+  def gen_line("U" <> length,[{x,y}|_] = acc) do
+    head = gen_end(y, "+", length)..y+1
+    |> Enum.map(&{x, &1})
+    head ++ acc
+  end
+  def gen_line("D" <> length,[{x,y}|_] = acc) do
+    head = gen_end(y, "-", length)..y-1
+    |> Enum.map(&{x, &1})
+    head ++ acc
   end
 end
--- test/aoc2019_test.exs
+++ test/aoc2019_test.exs
@@ -3,25 +3,11 @@ defmodule Aoc2019Test do
   import Aoc2019
   doctest Aoc2019
 
-  test "individual mass" do
-    assert Aoc2019.mass_rec(12) == 2
-    assert Aoc2019.mass_rec(14) == 2
-    assert Aoc2019.mass_rec(1969) == 966
-    assert Aoc2019.mass_rec(100756) == 50346
-    assert Aoc2019.mass(12) == 2
-    assert Aoc2019.mass(14) == 2
-    assert Aoc2019.mass(1969) == 654
-    assert Aoc2019.mass(100756) == 33583
-    assert Aoc2019.day1() == 3368364
-  end
-  test "day 2" do
-    assert Aoc2019.string_to_list("1,0,0,0,99")
-    |> Aoc2019.list_to_map == %{0 => 1, 1 => 0, 2 => 0, 3 => 0, 4 => 99}
-    assert Aoc2019.string_to_list("1,0,0,0,99")
-    |> Aoc2019.list_to_map
-    |> Aoc2019.day2_process(5) ==2 
-  end
-  test "day 2.2" do
-    assert Aoc2019.day2() == 19690720
+  test "day3" do
+
+    #assert day31("R2,D3,L3,U3\nD5,R1,U6") == 159
+    assert day31("R75,D30,R83,U83,L12,D49,R71,U7,L72\nU62,R66,U55,R34,D71,R55,D58,R83") == 159
+    assert day31("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51\nU98,R91,D20,R16,D67,R40,U7,R15,U6,R7") == 135
+    assert read_day31() == 399
   end
 end
